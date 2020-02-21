8e9eadd294fb9507439e444795ad3f1e0ce69213
webanh <mwh0808@hotmail.com>
2020-02-21 18:25:24 +0800

    DMS.Log4net 1.3.2020.0221 添加Source位置

--- src/DMS.Core/DMS.Log4net/Filters/GlobalExceptionFilter.cs
+++ src/DMS.Core/DMS.Log4net/Filters/GlobalExceptionFilter.cs
@@ -29,13 +29,13 @@ namespace DMS.Log4net.Filters
             //这里面是自定义的操作记录日志
             if (context.Exception.GetType() == typeof(UserOperationException))
             {
-                json.errmsg = $"用户自定义错误，Message:{context.Exception.Message} {context.Exception.InnerException?.Message}";
+                json.errmsg = $"用户自定义错误，Message:{context.Exception.Message} {context.Exception.InnerException?.Message} {context.Exception.InnerException?.Source}";
                 Logger.Error($"{json.errmsg}，StackTrace:{context.Exception.StackTrace} {context.Exception.InnerException?.Message} {context.Exception.InnerException?.StackTrace}");
                 context.Result = new BadRequestObjectResult(json);
             }
             else
             {
-                json.errmsg = $"内部错误，Message:{context.Exception.Message} {context.Exception.InnerException?.Message}";
+                json.errmsg = $"内部错误，Message:{context.Exception.Message} {context.Exception.InnerException?.Message} {context.Exception.InnerException?.Source}";
                 Logger.Error($"{json.errmsg}，StackTrace:{context.Exception.StackTrace} {context.Exception.InnerException?.Message} {context.Exception.InnerException?.StackTrace}");
                 context.Result = new InternalServerErrorObjectResult(json);
             }
